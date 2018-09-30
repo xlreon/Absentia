8e9abbbf48e2466380f1dd24a588bea946390897
Sidharth <sidharth.satapathy9@gmail.com>
2018-09-30 17:20:52 +0530

    Phone numbers extracted from given file.

--- server/index.js
+++ server/index.js
@@ -1,8 +1,25 @@
-var WebSocketServer = require('ws').Server,
+const WebSocketServer = require('ws').Server,
     wss = new WebSocketServer({port: 40510})
+const xlsx = require('node-xlsx');
+const regionFile = xlsx.parse('./data/nanp.xlsx')
+//console.log(getColumn(regionFile,1))
 
 wss.on('connection', (ws) => {
     ws.on('message', (file) => {
-        console.log(file)
+        //console.log(file)
+        console.log(getColumn(file,2))
+        //console.log(fileData[0].data[0])
     })
 })
+
+getColumn = (file,column) => {
+    var fileData = xlsx.parse(file)
+    var phoneNumbers = []
+    fileData[0].data.map((val,index) => {
+        if(index != 0) {
+            phoneNumbers.push(val[0])
+        }
+    })
+    return phoneNumbers;
+}
+
