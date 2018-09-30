864351027e2682a990d53af7fc5ce73d113d2606
Sidharth <sidharth.satapathy9@gmail.com>
2018-09-30 19:14:10 +0530

    Get Region function added for 1 Sheet of data.

--- server/index.js
+++ server/index.js
@@ -2,25 +2,36 @@ const WebSocketServer = require('ws').Server,
     wss = new WebSocketServer({port: 40510})
 const xlsx = require('node-xlsx');
 const regionFile = './data/nanp.xlsx'
-
+var fileData = [];
 wss.on('connection', (ws) => {
     ws.on('message', (file) => {
-        console.log(getColumn(file,2))
+        var numbers = getColumn(file,0)
+        getRegion(regionFile,numbers)
     })
 })
 
 getColumn = (file,column) => {
-    var fileData = [];
-    fileData = xlsx.parse(file) 
     var phoneNumbers = []
+    fileData = xlsx.parse(file) 
     fileData[0].data.map((val,index) => {
         if(index != 0) {
-            var firstFour = parseInt((val[0]+"").slice(0,4))
+            var firstFour = parseInt((val[column]+"").slice(0,4))
             if(!phoneNumbers.includes(firstFour)) {
                 phoneNumbers.push(firstFour)
             }
         }
     })
     return phoneNumbers;
+}
+
+getRegion = (regionFile,phoneNumbers) => {
+    var regionNumbers = getColumn(regionFile,0);
+    regionNumbers.map((val,index) => {
+        phoneNumbers.map((number,ind) => {
+            if(val === number) {
+                console.log(val,"-- Region --> ",fileData[0].data[index][2])
+            }
+        }) 
+    })
 }
 
