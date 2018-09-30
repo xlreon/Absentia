3e1216edbd78d7286ec64d86291a05c47f9a88a4
Sidharth <sidharth.satapathy9@gmail.com>
2018-09-30 17:34:39 +0530

    Data file parsing added.

--- server/index.js
+++ server/index.js
@@ -1,8 +1,7 @@
 const WebSocketServer = require('ws').Server,
     wss = new WebSocketServer({port: 40510})
 const xlsx = require('node-xlsx');
-const regionFile = xlsx.parse('./data/nanp.xlsx')
-//console.log(getColumn(regionFile,1))
+const regionFile = './data/nanp.xlsx'
 
 wss.on('connection', (ws) => {
     ws.on('message', (file) => {
@@ -13,7 +12,8 @@ wss.on('connection', (ws) => {
 })
 
 getColumn = (file,column) => {
-    var fileData = xlsx.parse(file)
+    var fileData = [];
+    fileData = xlsx.parse(file) 
     var phoneNumbers = []
     fileData[0].data.map((val,index) => {
         if(index != 0) {
