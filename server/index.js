f0b48081728609f0313038fe31a9a600e33e8682
Sidharth <sidharth.satapathy9@gmail.com>
2018-10-02 03:05:36 +0530

    Data filtering handled for all sheets in the file.

--- server/index.js
+++ server/index.js
@@ -7,35 +7,61 @@ var tree = new BST()
 var uploadData = []
 const regionFile = './data/nanp.xlsx'
 var phoneNumbers = [];
+var sheetLengths = [];
 var fileData = xlsx.parse(regionFile) 
-var regionNumbers = getColumn(regionFile,0);
-regionNumbers.map((val,index) => {
-    tree.insert(parseInt(val), index)
+var regionNumbers = getColumn(regionFile,0,"data");
+
+var count = 0
+var nodes = 0
+sheetLengths.map((val,index) => {
+    for(var i=count;i<=val;i++) {
+        tree.insert(parseInt(regionNumbers[nodes]), nodes-count)
+        nodes++;
+    }
+    count=val
 })
 
 var newData = []
 
 wss.on('connection', (ws) => {
     ws.on('message', (file) => {
-        //console.log(numbers)
-        var numbers = getColumn(file,0) 
+        var numbers = getColumn(file,0,"phone") 
         getRegion(numbers)
         createFile(0)
         ws.send(buffer)
     })
 })
 
-function getColumn(file,column) {
+function getColumn(file,column,profile) {
+
     phoneNumbers = []
+    var sum = 0
     uploadData = xlsx.parse(file)
-    uploadData[0].data.map((val,index) => {
-        if(index != 0) {
-            var firstFour = parseInt((val[column]+"").slice(0,4))
-            if(!phoneNumbers.includes(firstFour) && typeof(firstFour) === 'number') {
-                phoneNumbers.push(firstFour)
-            }
-        }
-    })
+    if(profile === 'data')
+    {
+        uploadData.map(value =>  {
+        sum += value.data.length-1
+        sheetLengths.push(sum)
+         value.data.map((val,index) => {
+             if(index != 0) {
+                 var firstFour = parseInt((val[column]+"").slice(0,4))
+                 if(!phoneNumbers.includes(firstFour) && typeof(firstFour) === 'number') {
+                     phoneNumbers.push(firstFour)
+                 }
+             }
+             })
+        })
+    }
+    else {
+     uploadData[0].data.map((val,index) => {
+         if(index != 0) {
+             var firstFour = parseInt((val[column]+"").slice(0,4))
+             if(!phoneNumbers.includes(firstFour) && typeof(firstFour) === 'number') {
+                 phoneNumbers.push(firstFour)
+             }
+         }
+         })
+    }
     return phoneNumbers;
 }
 
@@ -43,8 +69,13 @@ function getRegion(phoneNumbers) {
     phoneNumbers.map((number,ind) => {
         var result = tree.find(number)
         if(result) {
-            newData.push([number,fileData[0].data[parseInt(result)][2]])
-            //console.log(number,"-- Region --> ",fileData[0].data[parseInt(result)][2])
+            fileData.map((value) => {
+                if(value.data[parseInt(result)+1] !== undefined && value.data[result+1][0] === number)
+                {
+                    newData.push([number,value.data[parseInt(result)+1][2]])
+                    //console.log(number,"-- Region --> ",value.data[parseInt(result)+1][2])
+                }
+            })
         }
     })
 }
@@ -59,12 +90,10 @@ function createFile(column) {
                         var insertIndex = uploadData[0].data[0].indexOf('Country') 
                         if(insertIndex !== -1)
                         {
-                            console.log(insertIndex)
                             uploadData[0].data[index][insertIndex] = value[1]
                         }
                         else {
                             uploadData[0].data[index].push(value[1])
-                            console.log(uploadData[0].data[index])
                         }
                     }
                 }) 
